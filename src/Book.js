e6a940cab687bce76f181da8bbdf6b92376c727a
CRafael22 <carlosrafael093@gmail.com>
2017-12-17 17:30:19 -0300

    Adicionado getAll request do servidor ao logar pagina. Spinner esta sendo mostrado ao esperar retorno da requisicao do servidor. Component Book criado para modularizar codigo

---
+++ src/Book.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Book = (props) => {
+
+	return (
+		<div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: "url("+props.book_cover+")" }}></div>
+            <div className="book-shelf-changer">
+              <select>
+                <option value="none" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+          </div>
+          <div className="book-title">{props.book_title}</div>
+          <div className="book-authors">{props.book_authors}</div>
+        </div>
+	)
+}
+
+export default Book;
